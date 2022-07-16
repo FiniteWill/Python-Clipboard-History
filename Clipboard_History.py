324a9a0674fbcf6c6f9dddbe5edccacc4c9f8537
aaronduffey96@gmail.com <aaronduffey96@gmail.com>
2022-07-16 04:49:45 -0500

    Copying an object creates a new entry in the GUI window. There is a bug where copying text inside of the GUI from the generated entries will cause unexpected output on the next generated entry.

--- Clipboard_History.py
+++ Clipboard_History.py
@@ -18,23 +18,21 @@ clipboard = []
 recent_value = ""
     
 def thread_func():
+    # Creating local variant of global var to use
+    global recent_value
     while True:
-        # Creating local variant of global var to use
-        global recent_value
-        local_rv = recent_value
-
-        
         tmp_value = pyperclip.paste()
-        #print(str(tmp_value + " " + recent_value))
         if tmp_value != recent_value:
-            local_rv = tmp_value
-            print("Value changed: %s" % str(local_rv)[:40])
+            recent_value = tmp_value
 
             if len(clipboard) < clipboard_size:
-                clipboard.append(local_rv)
-                #label = tk.Label(root, text="ddeosdf")
-                #label.pack()
-            
+                clipboard.append(recent_value)
+                #label = tk.Label(root, text=recent_value)
+                label = tk.Text(root, height = 5, width = 30)
+                label.insert(tk.END, str(recent_value))
+                label.pack()
+            else:
+                print("Clipboard Full")
         time.sleep(0.5)
         
 if __name__ == '__main__':
@@ -42,21 +40,26 @@ if __name__ == '__main__':
     root = tk.Tk()
     root.geometry("500x250")
 
-    # Must take in 1 param (the event it was called from)
-    def handler(e):
-        print("test")
+    # Must take in 1 el.insert(tk.END, str(recent_value))
+                label.pack()
         newEntry = tk.Label(root, text=recent_value)
         newEntry.pack()
+        
     def handler_b(e):
         pyperclip.copy("Testing the copy mechanism")
         print("copied")
+        
+    def handler_clipboard(e):
+        for i in clipboard:
+            print(str(i))
               
     
     entry = tk.StringVar()
 
     # Bind values
-    root.bind('<Return>', handler)
-    root.bind('<Control-a>', handler_b)
+    #root.bind('<Return>', handler)
+    #root.bind('<Control-a>', handler_b)
+    #root.bind('<Control-b>', handler_clipboard)
 
     clipboard_detection = Thread(target=thread_func, args=())
     clipboard_detection.start()
