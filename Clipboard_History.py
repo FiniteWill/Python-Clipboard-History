1cbdfd0ba661e3638a2c5a397f26e51251a727d3
aaronduffey96@gmail.com <aaronduffey96@gmail.com>
2022-07-23 04:38:36 -0500

    Changed clipboard entries to be buttons which recopy selected entry. Disallowed duplicate entries (duplicates must be exact to be detected)

--- Clipboard_History.py
+++ Clipboard_History.py
@@ -18,51 +18,74 @@ clipboard_size = 30
 clipboard = []
 recent_value = ""
 root = tk.Tk()
-text = tk.Text(root, height=40, width=40)
+
+canvas_width = 200;
+canvas_height = 200;
+entry_canvas = tk.Canvas(root, bg="white", height=canvas_height, width=canvas_width)
+canvas_entries = []
+
+text = tk.Text(root, height=40, width=40, bg='#000000', fg='#FFFFFF')
+test_threading = 0
+
+def copy(cop):
+    recent_value = cop
+    pyperclip.copy(cop)
+    print(cop)
+def delete(entry):
+    clipboard.remove(entry)
     
 def thread_func():
     # Creating local variant of global var to use
     global recent_value
-    while True:
-        tmp_value = pyperclip.paste()
-        if tmp_value != recent_value:
-            recent_value = tmp_value
+    global test_threading
+    global canvas_entries
 
-            if len(clipboard) < clipboard_size:
+    while True:
+        test_threading+=1
+        temp_value = pyperclip.paste()
+        if temp_value != recent_value:
+            recent_value = temp_value 
+
+            # Check that there is room for the entry and that it is not already in clipboard
+            if len(clipboard) < clipboard_size and recent_value not in clipboard:
                 clipboard.append(recent_value)
-                label = tk.Label(root, text=recent_value)
-                label = tk.Text(root, height = 5, width = 30)
-                label.insert(tk.END, str(recent_value))
-                text.insert(f'1.0',
-                            str(recent_value + '\n'
-                                + '-------------------------------------------' + '\n'))
-                #label.grid(row=clipboard_size, column=0)
-                #label.pack()
-            else:
+                
+                y = 0
+                y = clipboard.index(str(temp_value))
+
+                canvas_entries.append(ttk.Button(entry_canvas, text=recent_value,
+                                         width=100, command=lambda x=temp_value:copy(x)))
+                canvas_entries[len(clipboard)-1].grid(row=len(clipboard), column=0) 
+      
+            elif len(clipboard) >= clipboard_size:
                 print("Clipboard Full")
+            else:
+                pass
+                #print("Item is already inside of clipboard!")
+
         time.sleep(0.5)
         
 if __name__ == '__main__':
     # GUI main window initialization
-    #root = tk.Tk()
+    root.title("Clipboard History")
     root.geometry("500x250")
-    root.config(bg='#F25252')
+    root.config(bg='#000000') #F25252 (Melon pink)
     
     root.grid()
     root.grid_columnconfigure(0, weight=1)
     root.grid_rowconfigure(0, weight=1)
-
-
-    # create the text widget
-    #text = tk.Text(root, height=10)
-    text.grid(row=0, column=0, sticky=tk.EW) #sticky=tk.ES
+    
+    #text.grid(row=0, column=0, sticky=tk.NSEW)
+    entry_canvas.grid(row=0, column=0, sticky=tk.EW)
+    
 
     # create a scrollbar widget and set its command to the text widget
     scrollbar = ttk.Scrollbar(root, orient='vertical', command=text.yview)
-    scrollbar.grid(row=0, column=1, sticky=tk.NS)
+    scrollbar.grid(row=2, column=1, sticky=tk.NS)
 
     #  communicate back to the scrollbar
     text['yscrollcommand'] = scrollbar.set
+    #entry_canvas['yscrollcommand'] = scrollbar.set
          
     def handler_b(e):
         pyperclip.copy("Testing the copy mechanism")
@@ -78,7 +101,7 @@ if __name__ == '__main__':
     # Bind values
     #root.bind('<Return>', handler)
     #root.bind('<Control-a>', handler_b)
-    #root.bind('<Control-b>', handler_clipboard)
+    root.bind('<Control-b>', handler_clipboard)
 
     clipboard_detection = Thread(target=thread_func, args=())
     clipboard_detection.start()
