a33a875c33f9ce88e3f588dbee3288b0d4eb06d9
aaronduffey96@gmail.com <aaronduffey96@gmail.com>
2022-07-25 03:25:22 -0500

    Fixed issue with GUI elements being placed incorrectly when copying after deletion. GUI elements now shift appropriately and unused code was deleted.

--- Clipboard_History.py
+++ Clipboard_History.py
@@ -33,36 +33,27 @@ def copy(cop):
     pyperclip.copy(cop)
     print(cop)
 def delete(entry, txt, button):
-    # Get the clipboard element of this entry and remove it 
-    y=clipboard.index(txt)
-    clipboard.remove(clipboard[y])
-    # Delete the GUI button for copying entry
-    canvas_entries[canvas_entries.index(entry)].destroy()
-    canvas_entries.remove(entry)
-    # Delete the GUI button for deleting an entry
-    del_buttons.remove(button)
-    button.destroy()
-    print('Clipboard after deletion: '+str(len(clipboard)))
-    '''     
-        clipboard.remove(txt)
     try:
         # Get the clipboard element of this entry and remove it 
         y=clipboard.index(txt)
+        clipboard.remove(clipboard[y])
         # Delete the GUI button for copying entry
         canvas_entries[canvas_entries.index(entry)].destroy()
         canvas_entries.remove(entry)
         # Delete the GUI button for deleting an entry
         del_buttons.remove(button)
         button.destroy()
-        #del_buttons[y].destroy()
-        #del_buttons.remove(del_buttons[y])
-        # TODO: Reorder the other GUI elements to fill empty space
-        
-        clipboard.remove(txt)
+        # Shift GUI elements
+        i=0
+        for x in range(0,len(clipboard)):
+            print('Clipboard Entry '+str(clipboard[x])+' row = '+str(i))
+            canvas_entries[x].grid(row=i, column=0)
+            del_buttons[x].grid(row=i, column=1)
+            i+=1
+
     except:
-        print('Value: '+str(entry)+' not found in clipboard')
-        print('Text = '+str(txt))
-    '''
+        print('Deletion of '+str(entry)+' with text '+str(txt)+'failed.')
+
 def thread_func():
     # Creating local variant of global var to use
     global recent_value
@@ -78,22 +69,20 @@ def thread_func():
             # Check that there is room for the entry and that it is not already in clipboard
             if len(clipboard) < clipboard_size and recent_value not in clipboard:
                 clipboard.append(recent_value)
-                
-                y = 0
-                y = clipboard.index(str(temp_value))
-
+ 
+                # Add GUI buttons for copying and deleting the new entry
                 canvas_entries.append(ttk.Button(entry_canvas, text=recent_value,
-                                width=100, command=lambda x=temp_value: copy(x)))
-                print("Clipboard after copying: "+str(len(clipboard)))
-                canvas_entries[len(clipboard)-1].grid(row=len(clipboard), column=0)
-                
-                test = ttk.Button(entry_canvas, text="Delete", width=10)
-                
-                test.configure(command=lambda x=canvas_entries[len(clipboard)-1],
-                            x2=recent_value, button=test: delete(x, x2, button))
+                                width=100, command=lambda text=temp_value: copy(text)))
 
-                test.grid(row=len(clipboard), column=1)
-                del_buttons.append(test)
+                canvas_entries[len(clipboard)-1].grid(row=len(clipboard), column=0)
+                # Creating Button before configuring command becuase del_button needs to
+                # have a refernce to itself to be able to destroy itself in delete()
+                del_button = ttk.Button(entry_canvas, text="Delete", width=10)
+                del_button.configure(command=lambda entry=canvas_entries[len(clipboard)-1],
+                            text=recent_value, button=del_button: delete(entry, text, button))
+                del_button.grid(row=len(clipboard), column=1)
+                del_buttons.append(del_button)
+                
             elif len(clipboard) >= clipboard_size:
                 print("Clipboard Full")
             else:
@@ -124,21 +113,16 @@ if __name__ == '__main__':
     text['yscrollcommand'] = scrollbar.set
     #entry_canvas['yscrollcommand'] = scrollbar.set
          
-    def handler_b(e):
-        pyperclip.copy("Testing the copy mechanism")
-        print("copied")
-        
-    def handler_clipboard(e):
-        for i in clipboard:
-            print(str(i))
-              
-    
+
+    def print_clipboard(e):
+        for i in range(0, len(clipboard)):
+            print('Clipboard ['+str(i)+'] '+str(clipboard[i]))
+            print(canvas_entries[i].grid_info())
+            
     entry = tk.StringVar()
 
     # Bind values
-    #root.bind('<Return>', handler)
-    #root.bind('<Control-a>', handler_b)
-    root.bind('<Control-b>', handler_clipboard)
+    root.bind('<Control-b>', print_clipboard)
 
     clipboard_detection = Thread(target=thread_func, args=())
     clipboard_detection.start()
